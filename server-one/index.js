2414ddfdaa8378fad0ac2d36e08f3c48a763275b
Johnkai2196 <johnkaic@metropolia.fi>
2021-10-29 11:55:28 +0300

    serving static files from public folder

--- server-one/index.js
+++ server-one/index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
+app.use(express.static('public'))
+app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
 app.get('/catinfo', (req, res) => {
