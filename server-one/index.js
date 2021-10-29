59358bf0d3072614413cf9749a5b2fcbc0ddbc67
Johnkai2196 <johnkaic@metropolia.fi>
2021-10-29 11:37:53 +0300

    add catinfo route

--- server-one/index.js
+++ server-one/index.js
@@ -5,6 +5,14 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-app.listen(port,()=>{
-  console.log(`Example app listening at http:localhost:${port}`)
-})
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    birthdate: '2010-12-25',
+    weight: 5,
+  };
+  res.json(cat);
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http:localhost:${port}`);
+});
